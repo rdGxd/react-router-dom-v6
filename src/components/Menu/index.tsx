e4f3ca05463224c3240cba4cf2ce471b86f150a0
Rodrigo Silva <seu_email@email.com>
2023-11-04 08:13:42 -0300

    Link

---
+++ src/components/Menu/index.tsx
@@ -0,0 +1,17 @@
+import { Link } from "react-router-dom";
+import "./styles.css";
+
+export const Menu = () => {
+  return (
+    <nav className="menu">
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,13 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import { About } from "./components/About";
 import { Home } from "./components/Home";
+import { Menu } from "./components/Menu";
 import "./styles/global.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
+      <Menu />
       <Routes>
         <Route index path="/" element={<Home />}></Route>
         <Route index path="/about" element={<About />}></Route>
